fc011012e6a9186b59f17d0becad2765f3f7ae01
brancokas <branimir.stankovic1@gmail.com>
2022-11-11 21:38:29 +0100

    Napravljen prazan repositorij

--- IzvorniKod/Royal Standard/src/main/java/hr/fer/progi/dao/KorisnikRepository.java
+++
@@ -1,7 +0,0 @@
-package hr.fer.progi.dao;
-
-import hr.fer.progi.entity.Korisnik;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface KorisnikRepository extends JpaRepository<Korisnik, Long> {
-}
